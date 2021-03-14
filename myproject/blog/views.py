d93a5eed397eed68ff9c1c494e4dad9d3158d3b4
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-03-14 17:35:59 +0900

    Add a writing pages for blog

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Blog
 
 def home(request):
-    blogs = Blog.objects
+    blogs = Blog.objects    # 전체 데이터
     return render(request, 'home.html', {'blogs' : blogs})
 
 def detail(request, blog_id):
-    blog_detail = get_object_or_404(Blog, pk = blog_id)
+    blog_detail = get_object_or_404(Blog, pk = blog_id)     # 해당 데이터
     return render(request, 'detail.html', {'blog_detail' : blog_detail})
+
+def add(request):
+    return render(request, 'add.html')
+
+def create(request):    # DB에 form으로 받은 데이터 추가
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
     path('blog/<int:blog_id>', blog.views.detail, name="detail"),
+    path('add/', blog.views.add, name="add"),
+
+    path('create/', blog.views.create, name="create"),
 ]
