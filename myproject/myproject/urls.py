c1cdf8830f0248b50d4f60ca29579cdf451bc033
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-03-22 17:48:46 +0900

    Add accounts page and apply base.html for all pages

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
+import accounts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
     path('blog/<int:blog_id>', blog.views.detail, name="detail"),
     path('add/', blog.views.add, name="add"),
+    
+    path('accounts/', include('accounts.urls')),		# url 상속 : accounts/~~의 URL은 accounts app폴더의 urls.py로 이동
 
     path('create/', blog.views.create, name="create"),
 ]
