54d8d0fe57bce04cc25ae9cb786fdf1f09487a19
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-03-23 20:56:11 +0900

    Add google login

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
 	path('login/', views.login, name="login"),
     path('signup/', views.signup, name="signup"),
     path('logout/', views.logout, name="logout"),
+    
+    # 소셜 로그인 관련
+    path('google/', include('allauth.urls')),
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -16,12 +16,23 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'accounts.apps.AccountsConfig',
+
+    # allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    
+    # provider	(소셜 로그인 회사들)
+    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.provider.kakaotalk',
+    # 'allauth.socialaccount.provider.facebook',
 ]
 
 MIDDLEWARE = [
@@ -98,8 +109,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+    #Needed to login by username in Django admin, regardless of 'allauth'
+    'django.contrib.auth.backends.ModelBackend',
+    
+    # 'allauth' specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
